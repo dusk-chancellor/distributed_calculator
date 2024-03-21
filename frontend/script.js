f24853ff0deca8ee8f885944d75bf5f386bbf883
dior <diorbenskhodji@gmail.com>
2024-03-22 00:54:25 +0500

    YO! POST and GET methods added, building REST API goes successfully. data is stored in memory for now, in the near future full database connections will be developed. Now, just enjoy how your expressions stored and viewed via front page :)

--- frontend/script.js
+++ frontend/script.js
@@ -7,20 +7,56 @@ document.addEventListener('DOMContentLoaded', function() {
         const expression = form.elements.expression.value; // Get the expression from the input
 
         // Send the expression to the backend
-        fetch('http://localhost:8080/expressions', {
+        fetch('http://localhost:8080/expression/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ expression: expression }),
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            if (response.headers.get('Content-Type') !== 'application/json') {
+                throw new Error('Response is not JSON');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log('Success:', data);
-            // Handle the response from the server
         })
         .catch((error) => {
             console.error('Error:', error);
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:8080/expression/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const expressionsDiv = document.getElementById('savedExpressions');
+        data.forEach(expression => {
+            const p = document.createElement('p');
+            p.textContent = `ID: ${expression.id},
+                Expression: ${expression.expression},
+                Date: ${expression.date},
+                Status: ${expression.status}`;
+            expressionsDiv.appendChild(p);
+        });
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+})
