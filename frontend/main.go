b48e7b8068f9e6c695e92c79bb7c7829996720c7
dior <diorbenskhodji@gmail.com>
2024-03-22 00:54:25 +0500

    YO! POST and GET methods added, building REST API goes successfully. data is stored in memory for now, in the near future full database connections will be developed. Now, just enjoy how your expressions stored and viewed via front page :)

--- frontend/main.go
+++ frontend/main.go
@@ -1,33 +1,92 @@
 package main
 
+// TODO: REST API
+// [ ] POST   /expressions/    - creates an expression, an id for it and saves it
+// [ ] GET    /expressions/    - returns a list of all expressions
+// [ ] GET    /expressions/:id - returns specific expression with specified id
+// [ ] DELETE /expressions/    - deletes all expressions
+// [ ] DELETE /expressions/:id - deletes expression by its id
+
 import (
+	"calculator_yandex/storage"
 	"encoding/json"
 	"fmt"
+	"log"
+	"mime"
 	"net/http"
+	"time"
 )
 
-type ExpressionReq struct {
-	Expression string `json:"expression"`
+type expressionServer struct {
+	store *storage.ExpressionStore
 }
 
-func SendExpressionHandler(w http.ResponseWriter, r *http.Request) {
-	var req ExpressionReq
-	err := json.NewDecoder(r.Body).Decode(&req)
+func NewExprServer() *expressionServer {
+	return &expressionServer{
+		store: storage.NewStorage(),
+	}
+}
+
+func (es *expressionServer) setExpressionHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling expression set on %s\n", r.URL.Path)
+	date := time.Now()
+
+	contentType := r.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
 	if err != nil {
+		log.Printf("ParseMediaType: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediaType != "application/json" {
+		log.Print("mediaType is not json")
+		http.Error(w, "expected Content-Type to be application/json", http.StatusUnsupportedMediaType)
 		return
 	}
 
-	fmt.Println("Received:", req.Expression)
+	jsonDec := json.NewDecoder(r.Body)
+	jsonDec.DisallowUnknownFields()
 
-	w.Write([]byte("Expression received"))
+	type RequestExpression struct {
+		Expression string `json:"expression"`
+	}
+
+	var re RequestExpression
+	if err := jsonDec.Decode(&re); err != nil {
+		log.Printf("Could not decode: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	es.store.SetExpression(re.Expression, date, "stored")
+	w.WriteHeader(http.StatusOK)
+}
+
+func (es *expressionServer) getAllExpressionsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling get all expressions on %s\n", r.URL.Path)
+
+	allExprs := es.store.GetAllExpressions()
+	jsoned, err := json.Marshal(allExprs)
+	if err != nil {
+		log.Printf("Marshalling json: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsoned)
 }
 
 func main() {
-	http.HandleFunc("/expressions", SendExpressionHandler)
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	mux := http.NewServeMux()
+	server := NewExprServer()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	mux.HandleFunc("POST /expression/", server.setExpressionHandler)
+	mux.HandleFunc("GET /expression/", server.getAllExpressionsHandler)
 
 	fmt.Println("Server is running on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		return
 	}
 }
